abe22806ae91598f999e3cbdd87cc0bbd526f63a
bercovein <bercovsky.nm@gmail.com>
2022-05-05 22:54:07 -0300

    model

---
+++ src/main/java/com/ccoins/Users/model/Owner.java
@@ -0,0 +1,16 @@
+package com.ccoins.Users.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Owner extends User{
+    private String name;
+    private String lastname;
+    private String password;
+}
